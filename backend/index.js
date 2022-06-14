176245512e3dcd2852e559bb5e23d24f4e5ffe64
Joshua Kiel Gaurano <joshuakielg@gmail.com>
2022-06-14 10:42:18 +0800

    feat: connected backend to database

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = process.env.PORT || "8000";
+
+mongoose.connect(process.env.DB_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}, (err) => {
+    if(err){
+        console.log(`Cannot connect to database: ${err}`);
+    }else{
+        console.log("Connected to database");
+    }
+})
 
 app.listen(port, () => {
     console.log(`Backend listening on http://localhost:${port}`);
