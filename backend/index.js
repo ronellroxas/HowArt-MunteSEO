365377e5778322585ffb865f22f9f9708eb3b67d
Joshua Kiel Gaurano <joshua_kiel_gaurano@dlsu.edu.ph>
2022-06-22 23:59:49 +0800

    update backend cors config

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,10 @@ mongoose.connect(process.env.DB_URL, {
 });
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/public", express.static("public"));
